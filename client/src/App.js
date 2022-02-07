32366a15412216e0f3f42dcaedc4bb3c1c7e841b
Abhishek <abhi.harry33@gmail.com>
2022-02-08 01:56:52 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hello from abhishek kumar</div>;
+
+export default App;
---
+++ client/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: 'INR 0 to INR 9',
+    array: [0, 9],
+  },
+  {
+    _id: 2,
+    name: 'INR 10 to INR 19',
+    array: [10, 19],
+  },
+  {
+    _id: 3,
+    name: 'INR 20 to INR 29',
+    array: [20, 29],
+  },
+  {
+    _id: 4,
+    name: 'INR 30 to INR 39',
+    array: [30, 39],
+  },
+  {
+    _id: 5,
+    name: 'More than 40',
+    array: [40, 99],
+  },
+];
